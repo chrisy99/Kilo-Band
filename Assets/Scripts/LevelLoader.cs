578a24e3333f5e6730c868e00ff1f7f797d10b57
chrisy99 <chrisyip82@gmail.com>
2023-04-03 22:36:15 +0100

    portal script added

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime;
+    public static LevelLoader Instance;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //else if (Instance != this)
+        //{
+        //    Destroy(GameObject);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transition.SetTrigger("Rest");
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    LoadNextLevel();
+        //}
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadPreviousLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    public void LoadSpecificLevel(string LevelName)
+    {
+        StartCoroutine(LoadLevel(LevelName));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        //Play Animation
+        transition.SetTrigger("Transition");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load
+        SceneManager.LoadScene(LevelIndex);
+    }
+
+    IEnumerator LoadLevel(string LevelName)
+    {
+        //Play Animation
+        transition.SetTrigger("Transition");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load
+        SceneManager.LoadScene(LevelName);
+    }
+}
