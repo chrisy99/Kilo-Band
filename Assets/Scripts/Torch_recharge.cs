83bd69211dcb983b51873677d1f036c96517c70d
BLLivingstone <78822389+BLLivingstone@users.noreply.github.com>
2023-04-03 11:37:55 +0100

    fixed some errors
    variables were called in wrong place

--- Assets/Scripts/Torch_recharge.cs
+++ Assets/Scripts/Torch_recharge.cs
@@ -2,36 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public float timeMax = 20;
-public float timeValue = time;
-public float timeSincePress;
-public float current;
-public float since;
-
-
-public bool timeRunning = false;
-public bool light = true;
-public bool pressed = false;
-public bool movement = true;
-
-public float maxCharge = 100f;   // Maximum charge level for the flashlight
-public float chargeRate = 5f;    // Rate at which the flashlight charges per button press
-public KeyCode chargeButton = KeyCode.R;   // Button to mash to charge the flashlight
-
-private Light flashlight;
-private float currentCharge;
-
-
-public Animator animator;
-byte recharge=0;
-
+/* 
+full charge [x]
+tourch off [x] 
+send stop moving []
+press R/ key to recharge [x]
+tourch on [x]
+animation finish []
+enable moving []
+reset charge value [x]
+*/
 
 
 public class Torch_recharge : MonoBehaviour{
-private Light flashlight;
+
+    public float timeMax = 20;
+    public float timeValue = time;
+    public float timeSincePress;
+    public float current;
+    public float since;
+
+
+    public bool timeRunning = false;
+    public bool light = true;
+    public bool pressed = false;
+    public bool movement = true;
+
+    public float maxCharge = 100f;   // Maximum charge level for the flashlight
+    public float chargeRate = 5f;    // Rate at which the flashlight charges per button press
+    public KeyCode chargeButton = KeyCode.R;   // Button to mash to charge the flashlight
+
+    private Light flashlight;
+    private float currentCharge;
+
+
+    public Animator animator;
+    byte recharge=0;
 
     void Start() {
-        timeRunning = true; // starts timer for torch battery
+       // timeRunning = true; // starts timer for torch battery
         flashlight = GetComponent<Light>(); // gets light component attached to torch
         movement = true;
         currentCharge = maxCharge;   // Start with a fully charged flashlight
